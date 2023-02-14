c9efe80cf54834b70b8e7354e5e54bb2ffa826a1
AndrewL777 <7landrew7@gmail.com>
2023-02-14 14:01:30 -0500

    123123

--- Higher or Lower/index.js
+++ Higher or Lower/index.js
@@ -1,24 +1,40 @@
 const Dom = {
   men: document.getElementById("men"),
   start: document.getElementById("start"),
-  higher: document.getElementById("higer"),
-  lower: document.getElementById("lower"),
+  high: document.getElementById("high"),
+  low: document.getElementById("low"),
 };
-
+const random = Math.floor(Math.random() * 101);
+const random2 = Math.floor(Math.random() * 101);
 // Math.floor(Math.random() * 101) random number 0-100
 
 function low() {
-  Math.floor(Math.random() * 101)
-    .document.getElementById("men")
-    .insertAdjacentHTML(
-      "afterend",
-      `
+  document.getElementById("men").insertAdjacentHTML(
+    "beforeend",
+    `
     <div class="card">
-    <h2 class="all">${5}</h2>
+    <h2>${random}</h2>
+    <button id=high> higher </button>
+    <button id=low> lower </button>
 
 </div>
 `
-    );
+  );
 }
-
 low();
+
+function higher() {
+  document.getElementById("men").insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="card">
+    <h2 class="small">${random2}</h2>
+  
+
+    
+
+</div>
+`
+  );
+}
+higher();
