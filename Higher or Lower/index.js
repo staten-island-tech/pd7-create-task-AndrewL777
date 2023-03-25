4cd3de37816ccaad235c163615b5ccc98a273a81
AndrewL777 <7landrew7@gmail.com>
2023-03-24 23:58:41 -0400

    Update index.js

--- Higher or Lower/index.js
+++ Higher or Lower/index.js
@@ -8,7 +8,6 @@ const Dom = {
 const history = [];
 let right = Math.floor(Math.random() * 101);
 let left = Math.floor(Math.random() * 101);
-const win = "Correct";
 let count = 0;
 
 
@@ -32,7 +31,9 @@ function cards() {
   );
 }
 cards();
-
+document.getElementById("low").addEventListener("click", function () {
+  lowBtn();
+});
 document.getElementById("high").addEventListener("click", function () {
   highBtn();
 });
@@ -43,15 +44,13 @@ function highBtn() {
     location.reload();
   } else {
     count++;
-    streak();
+    document.getElementById("streak").innerHTML = `Win Streak: ${count}`;
     replace();
 history.push(right);
-previous();
+document.getElementById("Hist").innerHTML = `Previous Number(s): ${history}`
   }
 }
-document.getElementById("low").addEventListener("click", function () {
-  lowBtn();
-});
+
 
 function lowBtn() {
   if (left > right) {
@@ -59,14 +58,12 @@ function lowBtn() {
     location.reload();
   } else {
     count++;
-    streak();
+    document.getElementById("streak").innerHTML = `Win Streak: ${count}`;
     replace();
     history.push(right);
-    previous();
+    document.getElementById("Hist").innerHTML = `Previous Number(s): ${history}`
   }
 }
-
-
 
 function replace () {
    right = Math.floor(Math.random() * 101);
@@ -76,11 +73,3 @@ function replace () {
 
 }
 
-function streak() {
-  document.getElementById("streak").innerHTML = `Win Streak: ${count}`;
-}
-
-
-function previous() {
-  document.getElementById("Hist").innerHTML = `Previous Number(s): ${history}`;
-}
